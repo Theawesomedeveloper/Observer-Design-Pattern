5f290d00361a3aca372d152a7ee08193c6c1eb12
Jonathan <113763057+Theawesomedeveloper@users.noreply.github.com>
2023-01-28 21:18:31 +0100

    Switched to correct file

--- observer.js
+++ observer.js
@@ -1,206 +1,67 @@
-/*
-Task & Assignment
-For this exercise, our goal is to create a telephone package. The telephone class should expose 3 different methods.
-
-AddPhoneNumber - For adding a new phone number
-RemovePhoneNumber - For removing a phone number
-DialPhoneNumber - For dialling a phone number (only phone numbers that have been added can be dialled)
-The telephone class should also maintain a list of #observers and notify them whenever a phone number is dialled. 
-
-
-
-Requirements
-
-Create a telephone class. It should expose 3 public methods - AddPhoneNumber, RemovePhoneNumber, and DialPhoneNumber.
-Update the telephone class, so it uses the observer pattern (have methods to add, remove and notify #observers)
-
-Create a class for the observer, it should have a method that can be called by the telephone class to notify it. 
-Have the telephone class notify the #observers any time a phone number is dialed. 
-Add two #observers to the telephone class
-
- The first one should print the phone number
-  The second one should print `Now Dialling 2347023232`
-
-  */
-
-
-
 
 /**
+ * This is the telephone class
  * @class
  */
 class Telephone {
 
-    /**
-     * @private
-     * @type {...number}
-     */
-
-    #contactList;
-
-    /**
-     * @private
-     * @type {...number}
-     */
-
     #observers;
 
-    _phoneNumber;
-
-
-
-
+    /**
+     * this initializes an empty set when an instance of the Telephone class is created
+     * @constructor
+     */
     constructor() {
-        this.#contactList = new Array();
-        this.#observers = new Array()
+        this.#observers = new Set()
     }
 
-
-
-
     /**
-     * adds a phone number to the observer array
-     * @param {number} phoneNumber
-     * @return {void} 
-     */
-
-    _addObserver(phoneNumber) {
-        this.#observers.push(phoneNumber);
-        this._notifyObserver()
-    }
-
-
-
-
-    /**
-     * @protected
-     * removes the first element in the observer array
-     */
-
-    _removeObserver() {
-        this.#observers.shift()
-    }
-
-
-
-
-    /**
-     * @protected
-     * call the update method in the observer class
-     */
-
-    _notifyObserver() {
-        this.#observers.forEach(phoneNumber => {
-            observer.update(phoneNumber);
-            this._removeObserver()
-        })
-    }
-
-
-
-
-    /**
-     * @protected
-     * @param {number} phoneNumber - phone number to find its index
-     * @returns {number} index - index of the phoneNumber passed in
-     */
-
-    _findContact(phoneNumber) {
-        return this.#contactList.findIndex(contact => contact === phoneNumber);
-    }
-
-
-
-    
-
-    /**
-     * 
-     * @param {*} phoneNumber 
-     * @returns {number} PhoneNumber
-     */
-
-    _checkAndConvertToNumber(phoneNumber){
-        if (typeof (phoneNumber) !== 'number') {
-            // what!!! Phone number is not a number ? how dare them
-            // lets assume it was a string or string of numbers seperated by dashes
-            phoneNumber = parseInt(phoneNumber.replaceAll('-', ""))
-
-            // What if after all the bashing phone number still isn't a number ?  well let's check
-            if (typeof (phoneNumber) !== 'number') throw new Error('Phone Number must be a Number')
-        }
-
-        return phoneNumber;
-    }
-
-
-
-
-
-    /**
-     * Adds a phone number to our contact list
      * @public
-     * @param {number} phoneNumber - Adds the Phone number to our contact list
+     * @param {object} contactInfo - an instance of the Contact class
+     * @this Telephone
+     * @returns 
      */
+    addPhoneNumber(contactInfo) {
 
-    addPhoneNumber(phoneNumber) {
+        this.#observers.add(contactInfo);
 
-        this._phoneNumber = this._checkAndConvertToNumber(phoneNumber);
-        
-        if(isNaN(this._phoneNumber)) throw new Error('Phone Number must be a Number');
-        
-        this.#contactList.push(this._phoneNumber);
+        console.log(`${contactInfo.name} added sucessfully`)
+
+        return this;
+    }
+
+    /**
+     * This removes a specified observer from the list of observers
+     * @param {object | string | number} contactInfo - information associated with the number to be removed from the list of observers
+     * @this Telephone
+     */
+    removePhoneNumber(contactInfo) {
+
+        this.#observers.forEach(observer => {
+            if (observer.phoneNumber === contactInfo || observer.name === contactInfo.name || observer.name === contactInfo) {
+                this.#observers.delete(observer);
+                console.log(`${contactInfo.name} removed from the list of observers`)
+            }
+        });
 
         return this;
     }
 
 
-
-
-
     /**
-     * Removes a phone number from our contact list
-     * @public
-     * @param {number} phoneNumber - phone number to remove
-     * @returns {string | this}
+     * This notifies an observer 
+     * @param {object | string | number} contactInfo - information associated with the number to be notified in the list of observers
+    * @this Telephone
      */
 
-    removePhoneNumber(phoneNumber) {
-
-        this._phoneNumber = this._checkAndConvertToNumber(phoneNumber);
-        if(isNaN(this._phoneNumber)) throw new Error('Phone Number must be a Number');
-
-        const requestedContact = this._findContact(phoneNumber)
-
-        if (requestedContact === -1) return ('phoneNumber not found');
-
-        this.#contactList.splice(requestedContact, 1);
-        
+    dialPhoneNumber(contactInfo) {
+        this.#observers.forEach(observer => {
+            if (observer.phoneNumber === contactInfo || observer.name === contactInfo.name || observer.name === contactInfo) {
+                observer.update(observer);
+            }
+        });
 
         return this;
-    }
-
-
-
-
-
-    /**
-     * @public
-     * @param {number} phoneNumber 
-     * @returns { string | this}
-     */
-
-    dialPhoneNumber(phoneNumber) {
-
-        this._phoneNumber = this._checkAndConvertToNumber(phoneNumber);
-        if(isNaN(this._phoneNumber)) throw new Error('Phone Number must be a Number');
-
-        const requestedContact = this._findContact(this._phoneNumber);
-
-        if (requestedContact === -1) return ('phoneNumber not found');
-
-        this._addObserver(this._phoneNumber);
-
-        return this
     }
 
 
@@ -208,21 +69,26 @@ class Telephone {
 
 
 /**
- * Creates a new observer
- * @class 
+ * This is the observer class
+ * @class
  */
-class Observer {
+class Contact {
+
+    constructor(name, phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 
     /**
-     * logs the dialled number to the console and also return the number just in case we need it
-     * @param {number} phoneNumber 
-     * @returns {number} the dialed phone Number 
+     * this listens for when an instance of this class is dialled
+     * @public
+     * @param {object} observer 
+     * @returns {object}  observer that called the method
      */
-    update(phoneNumber) {
+    update(observer) {
+        console.log(`Now dialling... ${observer.phoneNumber}`);
 
-        console.log(`Dialling... ${phoneNumber}`);
-
-        return `Dialling... ${phoneNumber}`
+        return observer;
     }
 }
 
@@ -230,22 +96,32 @@ class Observer {
 
 
 
-const telephone = new Telephone()
-const observer = new Observer()
+const telephone = new Telephone();
 
-telephone.addPhoneNumber('111-111-111')
-telephone.addPhoneNumber(5678)
-telephone.addPhoneNumber(9101)
-telephone.addPhoneNumber(1213)
-telephone.addPhoneNumber(1415)
-telephone.addPhoneNumber(1617)
+const james = new Contact('james', 09060145678)
+const bright = new Contact('bright', 08123456783)
+const bliss = new Contact('bliss', 08045632451)
 
-telephone.removePhoneNumber(5678)
+// Methods are also Chainable
 
-telephone.dialPhoneNumber('111-111-111')
-telephone.dialPhoneNumber(5678)
-telephone.dialPhoneNumber(9101)
-telephone.dialPhoneNumber(1213)
+telephone
+    .addPhoneNumber(james)
+    .addPhoneNumber(bright)
+    .removePhoneNumber(james)
+    .dialPhoneNumber(bright)
+    .addPhoneNumber(bliss)
 
 
-telephone.addPhoneNumber(5555).addPhoneNumber(6666).removePhoneNumber(6666).dialPhoneNumber(5555)
+// OR YOU CAN WRITE THEM AS YOU PLEASE
+
+// telephone.addPhoneNumber(james)
+// telephone.addPhoneNumber(bright)
+// telephone.addPhoneNumber(bliss)
+
+// telephone.removePhoneNumber(1234)
+// telephone.removePhoneNumber(bright)
+// telephone.removePhoneNumber('bliss')
+
+// telephone.dialPhoneNumber(1234)
+// telephone.dialPhoneNumber(bright)
+// telephone.dialPhoneNumber('bliss')
